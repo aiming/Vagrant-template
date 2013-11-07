e660f18c16af9a616a2bc7152268d2d99634867c
futoase <futoase@gmail.com>
2013-11-07 17:57:48 +0900

    Created: Added setting of secure forward client.

--- Sandbox-fluent-plugin-secure-forward/site-cookbooks/setup-fluentd/attributes/default.rb
+++ Sandbox-fluent-plugin-secure-forward/site-cookbooks/setup-fluentd/attributes/default.rb
@@ -1,3 +1,4 @@
+# server
 node.default['setup-fluentd']['server']['users'] = {
   kyoko_sakura: "umaibou",
   mami_tomoe: "mounanimo-kowakunai"
@@ -7,3 +8,11 @@ node.default['setup-fluentd']['server']['shared_key'] = "qb"
 node.default['setup-fluentd']['server']['self_hostname'] = 'server.bebe.test'
 node.default['setup-fluentd']['server']['cert_auto_generate'] = 'yes'
 node.default['setup-fluentd']['server']['authentication'] = 'yes'
+
+# client
+node.default['setup-fluentd']['client']['shared_key'] = "qb"
+node.default['setup-fluentd']['client']['self_hostname'] = 'client.bebe.test'
+
+node.default['setup-fluentd']['client']['servers'] = [
+  "192.168.33.200"
+]
