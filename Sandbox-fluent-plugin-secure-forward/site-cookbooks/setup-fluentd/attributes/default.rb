3da70899e7aed74b5801b6ebf8b57e5ee3ed53ed
futoase <futoase@gmail.com>
2013-11-07 16:52:10 +0900

    Modified: Update default.rb.

--- Sandbox-fluent-plugin-secure-forward/site-cookbooks/setup-fluentd/attributes/default.rb
+++ Sandbox-fluent-plugin-secure-forward/site-cookbooks/setup-fluentd/attributes/default.rb
@@ -1,4 +1,9 @@
-node.default['setup-fluentd']['client']['users'] = [
-  { kyoko_sakura: "umaibou" },
-  { mami_tomoe: "mounanimo-kowakunai" }
-]
+node.default['setup-fluentd']['client']['users'] = {
+  kyoko_sakura: "umaibou",
+  mami_tomoe: "mounanimo-kowakunai"
+}
+
+node.default['setup-fluentd']['client']['shared_key'] = "qb"
+node.default['setup-fluentd']['client']['self_hostname'] = 'server.bebe.test'
+node.default['setup-fluentd']['client']['cert_auto_generate'] = 'yes'
+node.default['setup-fluentd']['client']['authentication'] = 'yes'
