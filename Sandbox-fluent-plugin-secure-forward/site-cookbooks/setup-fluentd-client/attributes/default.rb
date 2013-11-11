8abf44f6768da993d7129fec30eaafaa5cdef355
futoase <futoase@gmail.com>
2013-11-11 18:40:46 +0900

    Modified: Added cookbook of disable-iptables, and improved cookbooks.

--- Sandbox-fluent-plugin-secure-forward/site-cookbooks/setup-fluentd-client/attributes/default.rb
+++ Sandbox-fluent-plugin-secure-forward/site-cookbooks/setup-fluentd-client/attributes/default.rb
@@ -1,3 +1,4 @@
 # client
 node.default['setup-fluentd']['client']['shared_key'] = "qb"
-node.default['setup-fluentd']['client']['self_hostname'] = 'client.bebe.test'
+node.default['setup-fluentd']['client']['self_hostname'] = '192.168.33.200'
+node.default['setup-fluentd']['client']['ip_addr'] = '192.168.33.200'
--- Sandbox-fluent-plugin-secure-forward/site-cookbooks/setup-fluentd-server/attributes/default.rb
+++ Sandbox-fluent-plugin-secure-forward/site-cookbooks/setup-fluentd-server/attributes/default.rb
@@ -5,7 +5,7 @@ node.default['setup-fluentd']['server']['users'] = {
 }
 
 node.default['setup-fluentd']['server']['shared_key'] = "qb"
-node.default['setup-fluentd']['server']['self_hostname'] = 'server.bebe.test'
+node.default['setup-fluentd']['server']['self_hostname'] = '192.168.33.201'
 node.default['setup-fluentd']['server']['ip_addr'] = '192.168.33.201'
 node.default['setup-fluentd']['server']['cert_auto_generate'] = 'yes'
 node.default['setup-fluentd']['server']['authentication'] = 'yes'
