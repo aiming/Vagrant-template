01601335e0f105658b840f1decf3c2a90026d2aa
futoase <futoase@gmail.com>
2013-11-11 13:02:21 +0900

    Modified: Improve of chef cookbooks.

--- Sandbox-fluent-plugin-secure-forward/site-cookbooks/setup-fluentd-client-config/attributes/default.rb
+++
@@ -1,3 +0,0 @@
-# client
-node.default['setup-fluentd']['client']['shared_key'] = "qb"
-node.default['setup-fluentd']['client']['self_hostname'] = 'client.bebe.test'
--- Sandbox-fluent-plugin-secure-forward/site-cookbooks/setup-fluentd-client-config/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'setup-fluentd-client'
-maintainer       'Aiming Inc.'
-maintainer_email 'info@aiming-inc.com'
-license          'MIT'
-description      'Installs/Configures setup-fluentd-client'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
