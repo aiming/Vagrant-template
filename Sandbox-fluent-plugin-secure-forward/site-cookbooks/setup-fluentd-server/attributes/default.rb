e83b95a3c7503607b1ce75395953ffaf6036677d
futoase <futoase@gmail.com>
2013-11-11 11:19:15 +0900

    Modified: Improvement of chef cookbooks.

---
+++ Sandbox-fluent-plugin-secure-forward/site-cookbooks/setup-fluentd-server/attributes/default.rb
@@ -0,0 +1,11 @@
+# server
+node.default['setup-fluentd']['server']['users'] = {
+  kyoko_sakura: "umaibou",
+  mami_tomoe: "mounanimo-kowakunai"
+}
+
+node.default['setup-fluentd']['server']['shared_key'] = "qb"
+node.default['setup-fluentd']['server']['self_hostname'] = 'server.bebe.test'
+node.default['setup-fluentd']['server']['ip_addr'] = '192.168.33.200'
+node.default['setup-fluentd']['server']['cert_auto_generate'] = 'yes'
+node.default['setup-fluentd']['server']['authentication'] = 'yes'
