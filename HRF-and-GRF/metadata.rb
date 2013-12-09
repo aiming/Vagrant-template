cee8714726963e4b34f98351bcfd325f7e681364
futoase <futoase@gmail.com>
2013-12-09 13:39:50 +0900

    Modified: Support of Berkshelf.

---
+++ HRF-and-GRF/metadata.rb
@@ -0,0 +1,8 @@
+name             '.'
+maintainer       'Aiming Inc.'
+maintainer_email 'info@aiming-inc.com'
+license          'MIT'
+description      'Installs/Configures .'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
